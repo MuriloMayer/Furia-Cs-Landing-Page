88bc215614b94dcaf33a517aa874a250e34b5e93
Murilo Mayer <murilomayer@live.com>
2024-08-23 10:54:09 -0300

    feat: progress in lines page

--- src/app/components/lines/lines.component.ts
+++ src/app/components/lines/lines.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 interface Profile {
-  name: string;
+  firstName: string;
+  lastName: string;
   nickname: string;
   username: string;
-  photo: string;
+  photo?: string;
   role: string;
 }
 
@@ -16,51 +17,65 @@ interface Profile {
 })
 export class LinesComponent implements OnInit {
 
-  profiles: Profile[] = [];
+  players: Profile[] = [];
+  coachs: Profile[] = [];
   currentLine: string = 'masc';
 
-  mascPlayers : Profile[] = [
-    { name: 'Gabriel Toledo', nickname: '"Fallen"', username: '@fallen', photo: 'assets/fallen.webp', role: 'AWPer | IGL' },
-    { name: 'Marcelo Cespedes', nickname: '"chelo"', username: '@cheloking', photo: 'assets/chelo.webp', role: 'Entry Fragger' },
-    { name: 'Yuri Santos', nickname: '"yuurih"', username: '@yuurih', photo: 'assets/yuurih.webp', role: 'Rifler' },
-    { name: 'Kaike Cerato', nickname: '"KSCERATO"', username: '@kscerato', photo: 'assets/kscerato.webp', role: 'Rifler' },
-    { name: 'Felipe Medeiros', nickname: '"skullz"', username: '@skullzcs', photo: 'assets/skullz.webp', role: 'Rifler' },
+  masc : {players : Profile[], coachs : Profile[] } = {
+    players : [
+    { firstName: 'Gabriel', lastName: 'Toledo',nickname : '"fallen"' , username: 'fallen', photo: 'assets/images/line-masc/fallen.webp', role: 'AWPer | IGL' },
+    { firstName: 'Marcelo', lastName: 'Cespedes', nickname: '"chelo"', username: 'cheloking', photo: 'assets/images/line-masc/chello.webp', role: 'Entry Fragger' },
+    { firstName: 'Yuri Santos', lastName : 'Santos', nickname: '"yuurih"', username: 'yuurih', photo: 'assets/images/line-masc/yuurih.webp', role: 'Rifler' },
+    { firstName: 'Kaike Cerato', lastName: 'Cerato', nickname: '"KSCERATO"', username: 'kscerato', photo: 'assets/images/line-masc/kscerato.webp', role: 'Rifler' },
+    { firstName: 'Felipe Medeiros',lastName: 'Medeiros', nickname: '"skullz"', username: 'skullzcs', photo: 'assets/images/line-masc/skullz.webp', role: 'Rifler' },
+    ],
+    coachs : [
+      { firstName: 'Sid', lastName: 'Macedo', nickname: '"sidde"', username: 'siddecs', role: 'Coach' },
+      { firstName: 'Viacheslav', lastName: 'Britvin', nickname: '"innersh1ne"', username: 'innersh1necsgo', role: 'Assitant Coach' },
+    ]
+  }
+
+  fem : {players : Profile[], coachs : Profile[] } = {
+    players : [
+    { firstName: 'Gabriela', lastName: 'Freindorfer', nickname: '"gabs"', username: 'gabsfreindorfer', photo: 'assets/images/line-fem/gabs.webp', role: 'Rifler' },
+    { firstName: 'Izabella', lastName: 'Bieberbach', nickname: '"izaa"', username: 'izaa_galle', photo: 'assets/images/line-fem/izaa.webp', role: 'Rifler | IGL' },
+    { firstName: 'Karina', lastName: 'Takahashi', nickname: '"kaahSENSEI"', username: 'kaahtak', photo: 'assets/images/line-fem/kaahSENSEI.webp', role: 'AWPer' },
+    { firstName: 'Bruna', lastName: 'Marvila', nickname: '"bizinha"', username: 'bizinhafps', photo: 'assets/images/line-fem/bizinha.webp', role: 'Rifler' },
+    { firstName: 'Lucia', lastName: 'Dubra', nickname: '"lulitenz"', username: 'ludubra', photo: 'assets/images/line-fem/lulitenz.webp', role: 'Entry Fragger' },
+  ],
+  coachs : [
+    { firstName: 'Jhonatan', lastName: 'Moura', nickname: '"jnt"', username: 'fpsjnt', role: 'Coach' },
   ]
+  }
 
-  mascCoachs : Profile[] = [
-    { name: 'Sid Macedo', nickname: '"sidde"', username: '@siddecs', photo: '', role: 'Coach' },
-    { name: 'Viacheslav Britvin', nickname: '"innersh1ne"', username: '@innersh1necsgo', photo: '', role: 'Assitant Coach' },
+
+  acad : {players : Profile[], coachs : Profile[] } = {
+    players : [
+    { firstName: 'Gustavo', lastName: 'Ribeiro', nickname: '"GYZER"', username: 'gugyzer', photo: 'assets/images/line-acad/GYZER.webp', role: 'Rifler | IGL' },
+    { firstName: 'Leonardo', lastName: 'Melo', nickname: '"mello"', username: 'mellocs_', photo: 'assets/images/line-acad/mello.webp', role: 'AWPer' },
+    { firstName: 'Max', lastName: 'Borges', nickname: '"jotag3"', username: 'maxinfive', photo: 'assets/images/line-acad/Jotag3.webp', role: 'Rifler' },
+    { firstName: 'João', lastName: 'Gabriel', nickname: '"max"', username: 'j0t4g3', photo: 'assets/images/line-acad/max.webp', role: 'Rifler' },
+    { firstName: 'Luiz', lastName: 'de Souza', nickname: '"souz4h"', username: 'souz4h_', photo: 'assets/images/line-acad/souz4h.webp', role: 'Rifler' },
+  ],
+  coachs : [
+    { firstName: 'Leonardo', lastName: 'Ferreira', nickname: '"msr"', username: 'leonardomsrfps', role: 'Coach' },
   ]
-
-  femPlayers : Profile[] = [
-    { name: 'Gabriela Freindorfer', nickname: '"gabs"', username: '@gabsfreindorfer', photo: 'assets/gabs.webp', role: 'Rifler' },
-    { name: 'Izabella Bieberbach', nickname: '"izaa"', username: '@izaa_galle', photo: 'assets/izaa.webp', role: 'Rifler | IGL' },
-    { name: 'Karina Takahashi', nickname: '"kaahSENSEI"', username: '@kaahtak', photo: 'assets/kaahSENSEI.webp', role: 'AWPer' },
-    { name: 'Bruna Marvila', nickname: '"bizinha"', username: '@bizinhafps', photo: 'assets/bizinha.webp', role: 'Rifler' },
-    { name: 'Lucia Dubra', nickname: '"lulitenz"', username: '@ludubra', photo: 'assets/lulitenz.webp', role: 'Entry Fragger' },
-  ]
-
-  femCoachs : Profile[] = [
-    { name: 'Jhonatan Moura', nickname: '"jnt"', username: '@fpsjnt', photo: '', role: 'Coach' },
-  ]
-
-  acadPlayers : Profile[] = [
-    { name: 'Gustavo Ribeiro', nickname: '"GYZER"', username: '@gugyzer', photo: 'assets/GYZER.webp', role: 'Rifler | IGL' },
-    { name: 'Leonardo Melo', nickname: '"mello"', username: '@mellocs_', photo: 'assets/mello.webp', role: 'AWPer' },
-    { name: 'Max Borges', nickname: '"jotag3"', username: '@maxinfive', photo: 'assets/Jotag3.webp', role: 'Rifler' },
-    { name: 'João Gabriel', nickname: '"max"', username: '@j0t4g3', photo: 'assets/max.webp', role: 'Rifler' },
-    { name: 'Luiz de Souza', nickname: '"souz4h"', username: '@souz4h_', photo: 'assets/souz4h.webp', role: 'Rifler' },
-  ]
-
-  acadCoachs : Profile[] = [
-  { name: 'Leonardo Ferreira', nickname: '"msr"', username: '@leonardomsrfps', photo: '', role: 'Coach' },
-  ]
+  }
 
 
+  constructor() {
 
-  constructor() { }
+
+  }
 
   ngOnInit(): void {
+    this.changeLine('masc');
+  }
+
+  changeLine(line: string) {
+    this.currentLine = line;
+    this.players = this[line].players;
+    this.coachs = this[line].coachs;
   }
 
 }
