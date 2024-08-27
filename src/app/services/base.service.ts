f4d2f2e6ec4e20f73479d228d8aee2c0bf254eb4
Murilo Mayer <murilomayer@live.com>
2024-08-26 22:28:31 -0300

    feat: :art: adding details

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -12,8 +12,7 @@ export interface news {
   title: string;
   subTitle : string;
   date: Date;
-  content: string;
-  tweetHTML: string;
+  img: string;
 }
 
 @Injectable({
@@ -23,21 +22,25 @@ export class BaseService {
   news: news[] = [
     {
       title:
-        '<h2><span class="furia" style="color: #bc9b40;">FURIA</span> fecha parceria com a Fallen Gear e anuncia entrada no setor de periféricos</h2>',
-      content: '<p>O Mouse Wireless Pantera <span class="furia" style="color: #bc9b40;">FURIA</span> e os Mousepads <span class="furia">FURIA</span> são os primeiros produtos do projeto de licenciamento e desenvolvimento em conjunto com a Fallen Gear, empresa fundada por Gabriel "FalleN" Toledo, capitão da equipe de Counter-Strike da <span class="furia">FURIA</span>. De acordo com a organização, os equipamentos foram projetados para "atender às necessidades dos gamers mais exigentes" e foram desenvolvidos com "a mais alta tecnologia e precisão, garantindo uma performance superior em todas as partidas". Os novos produtos estão disponíveis exclusivamente na Fallen Store, com preços que variam entre R$179 e R$599.</p>',
+        'FURIA fecha parceria com a Fallen Gear e anuncia entrada no setor de periféricos',
       date: new Date(2024, 7, 21),
       subTitle:
-        '<p>Nesta quarta-feira (21), a <span class="furia">FURIA</span> anunciou sua entrada no setor de periféricos. A organização fechou parceria com a Fallen Gear, marca brasileira de equipamentos gamer, para para produzir seus próprios produtos.</p>',
-      tweetHTML:
-        '<blockquote class="twitter-tweet" data-theme="dark" ><p lang="pt" dir="ltr">Cansou de pinar e passar vergonha no lobby?<br>O mouse ta prejudicando a gameplay?<br><br>Seus problemas acabaram!<br>Chegou o Kit mouse + mousepad FURIA &amp; <a href="https://twitter.com/fallengearbr?ref_src=twsrc%5Etfw">@fallengearbr</a>! 🔥<br><br>Garanta o seu agora na <a href="https://twitter.com/fallenstore?ref_src=twsrc%5Etfw">@fallenstore</a>! <a href="https://t.co/Lw0zaOpZnj">pic.twitter.com/Lw0zaOpZnj</a></p>&mdash; FURIA (@FURIA) <a href="https://twitter.com/FURIA/status/1826348744792834492?ref_src=twsrc%5Etfw">August 21, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>',
-    },
+        'Nesta quarta-feira (21), a FURIA no setor de periféricos. A organização fechou parceria com a Fallen Gear, marca brasileira de equipamentos gamer, para para produzir seus próprios produtos.',
+      img: 'assets/images/news/furia-fallengear.webp',   },
+      {
+        title:
+          'FURIA vira sobre Aurora para se manter viva na BetBoom Dacha Belgrade S2',
+        date: new Date(2024, 7, 26),
+        subTitle:
+          'Brasileiros aguardam por 9 Pandas ou Eternal Fire para duelarem por vaga nos playoffs do torneio',
+        img: 'assets/images/news/fallen-wins.jpg'  },
   ];
 
   mascGames: game[] = [
     {
       team1: 'FURIA',
       team2: '9Pandas',
-      date: new Date(2024, 7, 25, 16, 30, 0),
+      date: new Date(2024, 7, 27, 7, 30, 0),
       event: 'BetBoom Dacha',
       team1Logo: 'assets/images/logoFuria-400x400.png',
       team2Logo: 'assets/images/9pandas.png',
@@ -62,8 +65,8 @@ export class BaseService {
   femGames: game[] = [
     {
       team1: 'FURIA',
-      team2: 'A Definir',
-      date: new Date(2024, 8, 1),
+      team2: 'thekillaz fe',
+      date: new Date(2024, 7, 28, 17, 30),
       event: 'Upcoming',
       team1Logo: 'assets/images/logoFuria-400x400.png',
       team2Logo: 'assets/images/questionmark.png',
@@ -111,6 +114,6 @@ export class BaseService {
   }
 
   getNews(): news[] {
-    return this.news;
+    return this.news.sort((a, b) => b.date.getTime() - a.date.getTime());
   }
 }
